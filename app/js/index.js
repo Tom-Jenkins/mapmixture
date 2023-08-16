1dd9f1c3c1ff4afd8c5d0fb44a83f855f7fca319
Tom Jenkins <tom.l.jenkins@outlook.com>
2023-08-16 14:50:41 +0100

    Update index.js
    Remove redundant code.

--- app/js/index.js
+++ app/js/index.js
@@ -68,22 +68,3 @@ export function clearPlotOutput () {
     document.getElementById("app-map_plot_module-admixture_map").textContent = "";
 };
 
-
-// Function to render file name when load sample data button is clicked (TESTING ONLY)
-// export function renderSampleData (dataType) {
-//     if (dataType === "admixture") {
-//         document.querySelector("div.sidebar-nonparam-container > div:nth-child(3) > div:nth-child(1) > div > div.input-group > input").value = "admix_sample.csv";
-//     }
-//     if (dataType === "coords") {
-//         document.querySelector("div.sidebar-nonparam-container > div:nth-child(5) > div:nth-child(1) > div > div.input-group > input").value = "coords_sample.csv";
-//     }
-// };
-
-
-
-
-
-
-
-
-
