6b36c26fdc1a1b4e6abb8b88bd70b7bf221563e8
Jenkins <t.l.jenkins@exeter.ac.uk>
2023-11-16 15:59:13 +0000

    New launch shiny app file name

--- R/app_launch.R
+++ R/app_launch.R
@@ -1,12 +1,17 @@
-#' Run Mapmixture Shiny App
+#' Launch Shiny App
 #'
 #' @description
 #' Wrapper function used to start mapmixture shiny app.
 #'
-#' @param ... additional arguments to pass to `shiny::runApp` call.
+#' @param ... additional arguments passed to `shiny::runApp`.
 #'
 #' @export
+#' @examples
+#' if (interactive()){
+#'   launch_mapmixture(launch.browser = TRUE)
+#' }
 launch_mapmixture <- function(...) {
+
   shiny::shinyApp(
     ui = app_ui,
     server = app_server,
