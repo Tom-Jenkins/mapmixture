37036ea1bce870854352351c3561bf231ab92536
Jenkins <t.l.jenkins@exeter.ac.uk>
2024-04-11 13:41:48 +0100

    Update app_launch.R

--- R/app_launch.R
+++ R/app_launch.R
@@ -3,6 +3,17 @@
 #' @description
 #' Wrapper function used to start mapmixture shiny app.
 #'
+#' App tested with the following package versions:
+#'
+#' shiny v1.8.0
+#' shinyFeedback v0.4.0
+#' shinyjs v2.1.0
+#' shinyWidgets 0.8.4
+#' bslib 0.7.0
+#' colourpicker 1.3.0
+#' htmltools v0.5.8.1
+#' waiter 0.2.5
+#'
 #' @param ... additional arguments passed to `shiny::runApp`.
 #'
 #' @export
